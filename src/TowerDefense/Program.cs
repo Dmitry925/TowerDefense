149c4e3cca6d6d878f53269074145a650889af49
Dmitry925 <rogowdmitriy917@gmail.com>
2022-10-12 12:06:16 +0300

    Added empty SFML window

--- src/TowerDefense/Program.cs
+++ src/TowerDefense/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SFML.Graphics;
+using SFML.Window;
+
+window = new RenderWindow(new VideoMode(600, 400), "Tower Defence", Styles.Titlebar | Styles.Close);
+window.SetVerticalSyncEnabled(true);
+
+
+while (window.IsOpen)
+{
+    window.DispatchEvents();
+
+    window.Closed += _Close!;
+
+    window.Clear(Color.Black);
+
+    window.Display();
+}
+
+static void _Close(object sender, EventArgs e)
+{
+    window.Close();
+}
+
+public partial class Program
+{
+    static RenderWindow window;
+}
