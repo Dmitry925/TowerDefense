275eaeb8f715538fd64cbf85ae2a24fcc38a22ef
Dmitry925 <rogowdmitriy917@gmail.com>
2022-10-12 12:18:30 +0300

    Added Tile class

---
+++ src/TowerDefense/Tile.cs
@@ -0,0 +1,24 @@
+﻿using SFML.Graphics;
+using SFML.System;
+
+namespace TowerDefense
+{
+    public class Tile : Transformable, Drawable
+    {
+        public const int TILE_SIZE = 60;
+
+        public Shape Shape { get; init; }
+
+        public Tile()
+        {
+            Shape = new RectangleShape(new Vector2f(TILE_SIZE, TILE_SIZE));
+        }
+
+        public void Draw(RenderTarget target, RenderStates states)
+        {
+            states.Transform *= Transform;
+
+            target.Draw(Shape, states);
+        }
+    }
+}
